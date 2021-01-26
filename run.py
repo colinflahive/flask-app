d16582c7672ea0d8ca3f44b1e830343746619e39
colinflahive <cflahive@hotmail.com>
2021-01-26 21:20:32 +0000

    CI Practical Python Flask Framework Walkthrough Updates

--- run.py
+++ run.py
@@ -2,14 +2,11 @@ import os
 import json
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.route("/about")
 def about():
